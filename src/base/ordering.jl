405d4b2e2c27d845dee0e38554042207bb4aa886
user.email <michael.oliver.boyle@gmail.com>
2022-05-24 10:12:55 -0400

    Test more ordering options

--- src/base/ordering.jl
+++ src/base/ordering.jl
@@ -1,15 +1,31 @@
 @inline (==)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ==Y.iₓ && X.x==Y.x
 @inline (!=)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ!=Y.iₓ || X.x!=Y.x
+@inline (==)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ==0 && X.x==Y
+@inline (!=)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ!=0 || X.x!=Y
+@inline (==)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ==0 && X.x==Y
+@inline (!=)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ!=0 || X.x!=Y
 
 @inline (<)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ<Y.iₓ || (X.iₓ==Y.iₓ && X.x<Y.x)
 @inline (<=)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ<Y.iₓ || (X.iₓ==Y.iₓ && X.x<=Y.x)
 @inline (>=)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ>Y.iₓ || (X.iₓ==Y.iₓ && X.x>=Y.x)
 @inline (>)(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ>Y.iₓ || (X.iₓ==Y.iₓ && X.x>Y.x)
+@inline (<)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ==0 && X.x<Y
+@inline (<=)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ==0 && X.x<=Y
+@inline (>=)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ>0 || (X.iₓ==0 && X.x>=Y)
+@inline (>)(X::XNumber{T}, Y::S) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ>0 || (X.iₓ==0 && X.x>Y)
+@inline (<)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ>0 || (X.iₓ==0 && Y<X.x)
+@inline (<=)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ>0 || (X.iₓ==0 && Y<=X.x)
+@inline (>=)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ<0 || (X.iₓ==0 && Y>=X.x)
+@inline (>)(Y::S, X::XNumber{T}) where {T,S<:Union{Real,AbstractFloat}} = X.iₓ<0 || (X.iₓ==0 && Y>X.x)
 
 @inline isequal(X::XNumber{T}, Y::XNumber{S}) where {T,S} = isequal(X.iₓ, Y.iₓ) && isequal(X.x, Y.x)
 @inline isequal(X::XNumber{T}, Y::Real) where T = X.iₓ==0 && isequal(X.x, Y)
 @inline isequal(Y::Real, X::XNumber{T}) where T = X.iₓ==0 && isequal(X.x, Y)
+@inline isequal(X::XNumber{T}, Y::AbstractFloat) where T = X.iₓ==0 && isequal(X.x, Y)
+@inline isequal(Y::AbstractFloat, X::XNumber{T}) where T = X.iₓ==0 && isequal(X.x, Y)
 
 @inline isless(X::XNumber{T}, Y::XNumber{S}) where {T,S} = X.iₓ<Y.iₓ || (X.iₓ==Y.iₓ && isless(X.x, Y.x))
 @inline isless(X::XNumber{T}, Y::Real) where T = X.iₓ<0 || (X.iₓ==0 && isless(X.x, Y))
 @inline isless(Y::Real, X::XNumber{T}) where T = X.iₓ>0 || (X.iₓ==0 && isless(Y, X.x))
+@inline isless(X::XNumber{T}, Y::AbstractFloat) where T = X.iₓ<0 || (X.iₓ==0 && isless(X.x, Y))
+@inline isless(Y::AbstractFloat, X::XNumber{T}) where T = X.iₓ>0 || (X.iₓ==0 && isless(Y, X.x))
--- test/ordering.jl
+++ test/ordering.jl
@@ -1,51 +1,95 @@
 @testset verbose=true "Comparisons ($T)" for T in [Float16, Float32, Float64]
-    a = XNumber{T}(1.2, 1)
-    b = XNumber{T}(3.4, 1)
-    c = XNumber{T}(1.2, 2)
+    a = XNumber{T}(1.2, 0)
+    b = XNumber{T}(3.4, 0)
+    c = XNumber{T}(1.2, 1)
+    d = XNumber{T}(3.4, 1)
+    e = XNumber{T}(1.2, 2)
+    xnumbers = [a, b, c, d, e]
 
-    @test a == a
-    @test b == b
-    @test c == c
-    @test a != b
-    @test a != c
-    @test b != c
-    @test b != a
-    @test c != a
-    @test c != b
+    for x in xnumbers
+        @test x == x
+        for y in xnumbers
+            if x !== y
+                @test x != y
+            end
+        end
+    end
+    @test a == T(1.2)
+    @test b == T(3.4)
+    @test c != T(1.2)
+    @test d != T(3.4)
+    @test e != T(1.2)
 
-    @test a ≤ a
-    @test b ≤ b
-    @test c ≤ c
-    @test a ≤ b
-    @test a ≤ c
-    @test b ≤ c
-    @test a < b
-    @test a < c
-    @test b < c
-    @test a ≥ a
-    @test b ≥ b
-    @test c ≥ c
-    @test b ≥ a
-    @test c ≥ a
-    @test c ≥ b
-    @test b > a
-    @test c > a
-    @test c > b
+    for (i,x) in enumerate(xnumbers)
+        @test x ≤ x
+        @test x ≥ x
+        if x.iₓ == 0
+            @test x > x.x-T(0.1)
+            @test x ≥ x.x-T(0.1)
+            @test x ≥ x.x
+            @test x ≤ x.x
+            @test x ≤ x.x+T(0.1)
+            @test x < x.x+T(0.1)
+            @test x.x-T(0.1) < x
+            @test x.x-T(0.1) ≤ x
+            @test x.x ≤ x
+            @test x.x ≥ x
+            @test x.x+T(0.1) ≥ x
+            @test x.x+T(0.1) > x
+        elseif x.iₓ > 0
+            @test x > x.x
+            @test x ≥ x.x
+            @test x.x < x
+            @test x.x ≤ x
+        end
+        for y in xnumbers[i+1:end]
+            @test x ≤ y
+            @test y ≥ x
+            @test x < y
+            @test y > x
+        end
+    end
 
-    @test isequal(a, a)
-    @test isequal(b, b)
-    @test isequal(c, c)
-    @test !isequal(a, b)
-    @test !isequal(a, c)
-    @test !isequal(b, c)
-    @test !isequal(b, a)
-    @test !isequal(c, a)
-    @test !isequal(c, b)
+    for x in xnumbers
+        @test isequal(x, x)
+        if x.iₓ == 0
+            @test isequal(x, x.x)
+            @test isequal(x.x, x)
+        else
+            @test !isequal(x, x.x)
+            @test !isequal(x.x, x)
+        end
+        for y in xnumbers
+            if x !== y
+                @test !isequal(x, y)
+            end
+        end
+    end
 
-    @test isless(a, b)
-    @test isless(a, c)
-    @test isless(b, c)
-    @test !isless(b, a)
-    @test !isless(c, a)
-    @test !isless(c, b)
+    for (i,x) in enumerate(xnumbers)
+        @test !isless(x, x)
+        if x.iₓ == 0
+            @test !isless(x, x.x-T(0.1))
+            @test !isless(x, x.x-T(0.1))
+            @test !isless(x, x.x)
+            @test !isless(x, x.x)
+            @test isless(x, x.x+T(0.1))
+            @test isless(x, x.x+T(0.1))
+            @test isless(x.x-T(0.1), x)
+            @test isless(x.x-T(0.1), x)
+            @test !isless(x.x, x)
+            @test !isless(x.x, x)
+            @test !isless(x.x+T(0.1), x)
+            @test !isless(x.x+T(0.1), x)
+        elseif x.iₓ > 0
+            @test !isless(x, x.x)
+            @test !isless(x, x.x)
+            @test isless(x.x, x)
+        end
+        for y in xnumbers[i+1:end]
+            @test isless(x, y)
+            @test !isless(y, x)
+        end
+    end
+
 end
